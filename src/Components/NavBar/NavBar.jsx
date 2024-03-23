e1232d7bb9c2a736670bbac7215032bce833bcfc
Lajeesh <gokulam.9920@gmail.com>
2024-03-23 20:47:31 +0530

    navbar finished

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,22 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
 import { assets } from "../../assets/assets";
 
 const NavBar = () => {
+  const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
       <img src={assets.logo} alt="logo" className="logo" />
       <ul className="navmenu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Contact us</li>
-        <li>Mobile-App</li>
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("contactus")}
+          className={menu === "contactus" ? "active" : ""}
+        >
+          Contact us
+        </li>
+        <li
+          onClick={() => setMenu("mobileapp")}
+          className={menu === "mobileapp" ? "active" : ""}
+        >
+          Mobile-App
+        </li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="search" />
         <div className="navbar-search-icon">
           <img src={assets.basket_icon} alt="basket" />
-          <div className="dot"></div>
+          <div className="dot">
+            <p>2</p>
+          </div>
         </div>
         <button>Sign-in</button>
       </div>
