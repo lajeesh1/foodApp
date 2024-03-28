2f1bb5347fe720ef79466560ebc52f99f1c96285
Lajeesh <gokulam.9920@gmail.com>
2024-03-28 10:39:28 +0530

    router path created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Cart from "./Pages/Cart/Cart";
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
 
 const App = () => {
   return (
     <div className="app">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
     </div>
   );
 };
